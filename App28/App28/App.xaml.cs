1f31df44202291bec0fb1d0baa63e00ac4656fb0
Sean Sparkman <sean.sparkman@outlook.com>
2019-04-12 21:50:28 -0500

    Example of Blank Xamarin Forms project with conflicting SkiaSharp library

---
+++ App28/App28/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App28
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+#if DEBUG
+            HotReloader.Current.Start(this);
+#endif
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
